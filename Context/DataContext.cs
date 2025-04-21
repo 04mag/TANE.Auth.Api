604b77b9b2c88112789e8da0984b0cedc123c458
04mag <mathiasgreisen@gmail.com>
2025-04-21 11:38:39 +0200

    Added DataContext

---
+++ Context/DataContext.cs
@@ -0,0 +1,71 @@
+﻿using TANE.Auth.Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace TANE.Auth.Api.Context
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            string adminRoleId = Guid.NewGuid().ToString();
+            string userRoleId = Guid.NewGuid().ToString();
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Id = adminRoleId, Name = "Admin", NormalizedName = "ADMIN".ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() },
+                new IdentityRole { Id = userRoleId, Name = "User", NormalizedName = "USER".ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() }
+            );
+
+            //Add admin user
+            string userId = Guid.NewGuid().ToString();
+            string username = "Admin";
+            string email = "Admin";
+
+            var user = new ApplicationUser
+            {
+                Id = userId,
+                UserName = username,
+                NormalizedUserName = username.ToUpper(),
+                Email = email,
+                NormalizedEmail = email.ToUpper(),
+                EmailConfirmed = true,
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+
+            //Hash the password for admin user
+            PasswordHasher<ApplicationUser> ph = new PasswordHasher<ApplicationUser>();
+
+            user.PasswordHash = ph.HashPassword(user, "Admin");
+
+            //seed admin user
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+
+            //Add admin to admin role
+            var adminRole = new IdentityUserRole<string>
+            {
+                RoleId = adminRoleId,
+                UserId = userId
+            };
+
+            //Add admin to user role
+            var userRole = new IdentityUserRole<string>
+            {
+                RoleId = userRoleId,
+                UserId = userId
+            };
+
+            //Seed roles to admin user
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(adminRole);
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(userRole);
+        }
+    }
+}
